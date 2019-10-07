54bf03d7a3f79ea447d5ac16a072b52339422a21
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-10-07 02:54:26 -0700

    Update Main.java

--- source-projects/simple-jar/src/main/java/com/datica/demo/Main.java
+++ source-projects/simple-jar/src/main/java/com/datica/demo/Main.java
@@ -2,6 +2,6 @@ package com.datica.demo;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello v1 from the JAR file: " + new java.util.Date());
+		logger.log("Hello v1 from the JAR file: " + new java.util.Date());
 	}
 }
